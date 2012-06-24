23eb3670fcb33bdd4b8d9439739d51bfc7e7043b
Eugene Ma <edma2@san.rr.com>
2012-06-24 12:47:56 -0700

    removed line noise

--- scala/q1.scala
+++ scala/q1.scala
@@ -1,5 +1,5 @@
 object Test {
     def main(args: Array[String]) {
-        println((0 until 1000 filter {n => n % 3 == 0 || n % 5 == 0}).sum)
+        println(0 until 1000 filter {n => n % 3 == 0 || n % 5 == 0} sum)
     }
 }
